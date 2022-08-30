0d18ce4d25dd1ef4f3bb99929097f4202f9417b7
sheikh-itech <sheikh.itech123@gmail.com>
2022-08-31 01:26:13 +0530

    Adding Cloud Config Server, to read properties remotely

---
+++ config-micro-service/src/main/java/com/config/service/ConfigurationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.config.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationDemoApplication 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(ConfigurationDemoApplication.class, args);
+    }
+}
---
+++ config-micro-service/src/main/java/com/config/service/resources/ConfigController.java
@@ -0,0 +1,59 @@
+package com.config.service.resources;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.config.service.config.DatabaseConfig;
+
+@RestController
+public class ConfigController {
+
+	@Value("${my.configuration.start}")
+	private boolean startMyConfiguration;
+	
+	@Value("${my.configuration.greeting}")
+	private String configGreeting;
+	
+	@Value("${my.configuration.greeting.person}")
+	private String configGreetingPerson;
+	
+	@Value("#{${my.configuration.db.detail}}")
+	private Map<String, String> dbConfig;
+	
+	@Value("${undefinedProperty:undefinedProperty}")
+	private String undefinedProperty;
+	
+	@Value("${my.configuration.list}")
+	private List<Integer> myList;
+	
+	@Autowired
+	private DatabaseConfig databaseConfig; //Using @ConfigurationPropeties(prefix)
+	
+	@Autowired
+	private Environment env;
+	
+	@RequestMapping(value="/config")
+	public void startConfiguration() {
+		
+		if(startMyConfiguration) {
+			System.out.println(configGreeting);
+			System.out.println(configGreetingPerson);
+			System.out.println(dbConfig);
+			System.out.println(undefinedProperty);
+			System.out.println(myList);
+			System.out.println(databaseConfig);
+		}
+	}
+	
+	@RequestMapping(value="/env")
+	public String startEnvironmentConf() {
+		
+		return env.toString();
+	}
+}
